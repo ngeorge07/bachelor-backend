412debeed5fd2e0e613c7fa27ed404753f9d5041
George <georgenico9@gmail.com>
2025-01-04 22:46:31 +0100

    implement authentication module with signup and login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Request,
+  ConflictException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Public } from './public.decorator';
+import {} from '@nestjs/common';
+import { SignInDto } from './dto/signIn.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.authService.signUp(createUserDto);
+    } catch (error) {
+      throw new ConflictException('User already exists');
+    }
+  }
+
+  @Public()
+  @Post('login')
+  async login(@Body() signInDto: SignInDto) {
+    const token = (
+      await this.authService.login(signInDto.email, signInDto.password)
+    ).access_token;
+
+    if (token) {
+      return this.authService.login(signInDto.email, signInDto.password);
+    }
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
