b0910788d862067596622c46740f9c594856d2bb
George <georgenico9@gmail.com>
2025-01-04 23:28:08 +0100

    refactor: remove DatabaseModule and configure Mongoose connection using environment variable

--- src/database/database.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
-@Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/my_train')],
-})
-export class DatabaseModule {}
